6aefd7bc9fcf3a884599eb4eb8cd109e220356bc
Andre Almar <andre@y7mail.com>
2016-04-25 16:41:53 -0300

    removed django_extensions from settins.py

--- eventex/settings.py
+++ eventex/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'test_without_migrations',
-    'django_extensions',
     'eventex.core',
     'eventex.subscriptions',
 ]
